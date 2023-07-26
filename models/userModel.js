7b41b5591cdc97e95ad0d147fda9c15c6a778270
Rajnish <RajnishAryan12@gmail.com>
2023-07-26 20:11:55 +0530

    password check added

--- models/userModel.js
+++ models/userModel.js
@@ -4,10 +4,8 @@ const User = mongoose.Schema;
 const UserSchema = new User({
     name: { type: String, required:[true ,"name is required"]},
     email: { type: String, required:[true ,"email is required"]},
-    password:{ type:String,
-       required:[true ,"password is required"]
+    password:{ type:String,required:[true ,"password is required"]},
   
-  },
     isAdmin:{
       type:Boolean,
       default:false
