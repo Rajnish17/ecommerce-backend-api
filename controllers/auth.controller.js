806beeddadf3e1fb03b87b52381b9d3789cc1a60
Rajnish <RajnishAryan12@gmail.com>
2023-07-27 20:09:07 +0530

    jsonwebToken added

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,39 +1,40 @@
 const User = require("../models/userModel");
 const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
 
 //Create a new user
 const register = async (req, res) => {
   const { name, email, password } = req.body;
-    try {
+  try {
 
-        //find existing user by email
-        const existingEmail =await User.findOne({email});
-        if(existingEmail){
-            return res.status(401).json({error:"email already exist"})
-        }
-        //check password length
-        if(password.length < 8){
-          return res.status(401).json({error:"password length should be greater than eight character"})
-        }
-        // Hash the password
-        const hashedPassword = await bcrypt.hash(password, 10);
-        // Create a new user
-        const newUser = new User({
-            name,
-            email,
-            password: hashedPassword,
-        });
+    //find existing user by email
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(401).json({ error: "email already exist" })
+    }
+    //check password length
+    if (password.length < 8) {
+      return res.status(401).json({ error: "password length should be greater than eight character" })
+    }
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // Create a new user
+    const newUser = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
 
-        await newUser.save();
+    await newUser.save();
 
-        res.status(201).json({
-           message: 'User registered successfully',
-           data:newUser
-           });
+    res.status(201).json({
+      message: 'User registered successfully',
+      data: newUser
+    });
 
-    } catch (err) {
-        res.status(500).json(err.message)
-    };
+  } catch (err) {
+    res.status(500).json(err.message)
+  };
 
 }
 
@@ -41,35 +42,41 @@ const register = async (req, res) => {
 //login
 const login = async (req, res) => {
   const { email, password } = req.body;
-    try {
-        //find user by email
-        const user = await User.findOne({ email });
-        if (!user) {
-          return res.status(401).json({ error: "user not found" });
 
-        };
+  try {
+    //find user by email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ error: "user not found" });
 
-        // Check the password
-        const passwordMatch = await bcrypt.compare(password, user.password);
-        if (!passwordMatch) {
-            return res.status(401).json({ error: 'Invalid  password' });
-        }
+    };
 
-        res.status(200).json({ 
-          message: 'Login successful',
-          user:user
-         });
-
-
-    } catch (err) {
-        console.log(err)
+    // Check the password
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ error: 'Invalid  password' });
     }
 
+    //generate access Token Here
+    const accessToken = jwt.sign({
+      id: user._id,
+      isAdmin: user.isAdmin
+    }, process.env.JWT_SECRET, {
+      expiresIn: "1d"
+    })
+
+    res.status(200).json({
+      message: 'Login successful',
+      user:user,
+      token:accessToken
+    });
+
+
+  } catch (err) {
+    console.log(err)
+  }
+
 }
-
-
-
-
 
 
 module.exports = {
---
+++ controllers/verifyToken.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+
+
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers.token;
+
+    if (authHeader) {
+        const token = authHeader.split(" ")[1];
+        jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+            if (err) res.status(403).json("wrong Token");
+
+            req.user = user;
+
+            next();
+        })
+    } else {
+        return res.status(401).json("you are not authorised")
+
+    }
+}
+
+
+module.exports ={
+    verifyToken
+}
