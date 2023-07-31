da308df17f050701481682bdba64c86a945e45cd
Rajnish <RajnishAryan12@gmail.com>
2023-07-31 10:50:17 +0530

    email service added

---
+++ controllers/email.controller.js
@@ -0,0 +1,33 @@
+const nodemailer = require('nodemailer');
+
+// Replace these values with your actual email configuration
+const emailConfig = {
+  service: 'Gmail',
+  auth: {
+    user: 'your_email@example.com',
+    pass: 'your_email_password'
+  }
+};
+
+const transporter = nodemailer.createTransport(emailConfig);
+
+// Function to send an email
+const sendEmail = async (req, res) => {
+  const { to, subject, text } = req.body;
+
+  try {
+    await transporter.sendMail({
+      from: emailConfig.auth.user,
+      to,
+      subject,
+      text
+    });
+
+    res.status(200).json({ message: 'Email sent successfully!' });
+  } catch (error) {
+    console.error('Error sending email:', error);
+    res.status(500).json({ error: 'Failed to send email.' });
+  }
+};
+
+module.exports =sendEmail
